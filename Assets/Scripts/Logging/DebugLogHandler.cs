5e2687d28b00e979dea0e0250e55f1b33d4045d6
KarenKrill <vampart1997@gmail.com>
2025-03-03 19:04:56 +0500

    Added ILogger implementations

---
+++ Assets/Scripts/Logging/DebugLogHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace KarenKrill.Logging
+{
+    internal class DebugLogHandler : ILogHandler
+    {
+        public void LogException(Exception exception, UnityEngine.Object context)
+        {
+            Debug.LogException(exception, context);
+        }
+
+        public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args)
+        {
+            Debug.LogFormat(logType, LogOption.None, context, format, args);
+        }
+    }
+}
