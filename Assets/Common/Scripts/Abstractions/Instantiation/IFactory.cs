04302753837f09206442f2f82886416a56c6d9ae
KarenKrill <vampart1997@gmail.com>
2025-03-26 19:49:04 +0500

    Added base factory interfaces

---
+++ Assets/Common/Scripts/Abstractions/Instantiation/IFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace KarenKrill.Common.Instantiation.Abstractions
+{
+    public interface IFactory { }
+    public interface IFactory<out ProducedType> : IFactory
+    {
+        ProducedType Create();
+    }
+    public interface IFactory<out ProducedType, in ConsumedType> : IFactory
+    {
+        ProducedType Create(ConsumedType value);
+    }
+    public interface IBaseFactory<in ProducedTypeBase> : IFactory where ProducedTypeBase : class
+    {
+        ProducedType Create<ProducedType>() where ProducedType : ProducedTypeBase;
+    }
+}
