681e561a56742724f02e5a0ebbd263f5c7d5879f
KarenKrill <vampart1997@gmail.com>
2025-03-27 20:01:00 +0500

    Added managed state machine implementation

---
+++ Assets/TheLabyrinth/Scripts/StateMachine/ManagedStateMachine.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KarenKrill.TheLabyrinth.StateMachine
+{
+    using Abstractions;
+
+    public class ManagedStateMachine<T> : IManagedStateMachine<T> where T : Enum
+    {
+        public IStateMachine<T> StateMachine { get; }
+
+        public ManagedStateMachine(IStateMachine<T> stateMachine, IEnumerable<IStateHandler<T>> stateHandlers, T initialState)
+        {
+            StateMachine = stateMachine;
+            _initialState = initialState;
+            _stateHandlers = stateHandlers.ToDictionary(stateHandler => stateHandler.State, stateHandler => stateHandler);
+        }
+
+        public void AddStateHandler(IStateHandler<T> stateHandler)
+        {
+            _stateHandlers[stateHandler.State] = stateHandler;
+        }
+
+        public void RemoveStateHandler(T state)
+        {
+            _stateHandlers.Remove(state);
+        }
+
+        public void Start()
+        {
+            StateMachine.StateEnter += OnStateEnter;
+            StateMachine.StateExit += OnStateExit;
+            StateMachine.TransitTo(_initialState);
+        }
+
+        private void OnStateEnter(IStateMachine<T> stateMachine, T state)
+        {
+            _stateHandlers[state].Enter();
+        }
+
+        private void OnStateExit(IStateMachine<T> stateMachine, T state)
+        {
+            _stateHandlers[state].Exit();
+        }
+
+        private T _initialState;
+
+        private Dictionary<T, IStateHandler<T>> _stateHandlers;
+    }
+}
