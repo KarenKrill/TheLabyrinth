6b19ce622e3f2941eff05f9f99156a1cdbec6812
KarenKrill <vampart1997@gmail.com>
2025-03-27 19:35:26 +0500

    Added managed state machine interface (with state handlers)

---
+++ Assets/TheLabyrinth/Scripts/Abstractions/StateMachine/IManagedStateMachine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KarenKrill.TheLabyrinth.StateMachine.Abstractions
+{
+    public interface IManagedStateMachine<T> where T : Enum
+    {
+        IStateMachine<T> StateMachine { get; }
+
+        void AddStateHandler(IStateHandler<T> stateHandler);
+
+        void RemoveStateHandler(T state);
+
+        void Start();
+    }
+}
--- Assets/TheLabyrinth/Scripts/Abstractions/StateMachine/IStateMachine.cs
+++ Assets/TheLabyrinth/Scripts/Abstractions/StateMachine/IStateMachine.cs
@@ -6,13 +6,19 @@ namespace KarenKrill.TheLabyrinth.StateMachine.Abstractions
     public delegate void StateTransitionDelegate<T>(IStateMachine<T> stateMachine, T state) where T : Enum;
     public interface IStateMachine<T> where T : Enum
     {
-        public T State { get; }
+        T State { get; }
+
         public event StateTransitionDelegate<T> StateEnter;
+
         public event StateTransitionDelegate<T> StateExit;
-        public IEnumerable<T> ValidStateTransitions(T state);
+
+        IEnumerable<T> ValidStateTransitions(T state);
+
         bool IsCanTransitTo(T state);
+
         /// <exception cref="InvalidStateMachineTransitionException"></exception>
         void TransitTo(T state);
+
         bool TryTransitTo(T state);
     }
 }
