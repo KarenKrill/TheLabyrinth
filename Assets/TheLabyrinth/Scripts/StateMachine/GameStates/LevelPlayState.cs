1c75bf2ab565693c6bc30417884fc1fe102ba525
KarenKrill <vampart1997@gmail.com>
2025-03-28 17:43:24 +0500

    Added missing game states

---
+++ Assets/TheLabyrinth/Scripts/StateMachine/GameStates/LevelPlayState.cs
@@ -0,0 +1,33 @@
+﻿namespace KarenKrill.TheLabyrinth.GameStates
+{
+    using StateMachine.Abstractions;
+    using GameFlow.Abstractions;
+    using UnityEngine;
+
+    public class LevelPlayState : IStateHandler<GameState>
+    {
+        public GameState State => GameState.LevelPlay;
+
+        public LevelPlayState(ILogger logger, IGameFlow gameFlow, IGameController gameController)
+        {
+            _logger = logger;
+            _gameFlow = gameFlow;
+            _gameController = gameController;
+        }
+
+        public void Enter()
+        {
+            _logger.Log($"{GetType().Name}.{nameof(Enter)}()");
+            _gameController.OnLevelPlay();
+        }
+
+        public void Exit()
+        {
+            _logger.Log($"{GetType().Name}.{nameof(Exit)}()");
+        }
+
+        private readonly ILogger _logger;
+        IGameFlow _gameFlow;
+        IGameController _gameController;
+    }
+}
---
+++ Assets/TheLabyrinth/Scripts/StateMachine/GameStates/LooseMenuState.cs
@@ -0,0 +1,51 @@
+﻿namespace KarenKrill.TheLabyrinth.GameStates
+{
+    using StateMachine.Abstractions;
+    using GameFlow.Abstractions;
+    using KarenKrill.Common.UI.Presenters.Abstractions;
+    using KarenKrill.Common.UI.Views.Abstractions;
+    using KarenKrill.TheLabyrinth.UI.Views.Abstractions;
+    using UnityEngine;
+
+    public class LooseMenuState : IStateHandler<GameState>
+    {
+        public GameState State => GameState.LooseMenu;
+
+        public LooseMenuState(ILogger logger,
+            IViewFactory viewFactory,
+            IPresenter<ILooseMenuView> looseMenuPresenter,
+            IPresenter<IILevelInfoView> levelInfoPresenter,
+            IGameController gameController)
+        {
+            _logger = logger;
+            _viewFactory = viewFactory;
+            _looseMenuPresenter = looseMenuPresenter;
+            _levelInfoPresenter = levelInfoPresenter;
+            _gameController = gameController;
+        }
+
+        public void Enter()
+        {
+            _logger.Log($"{GetType().Name}.{nameof(Enter)}()");
+            _looseMenuPresenter.View ??= _viewFactory.Create<ILooseMenuView>();
+            _looseMenuPresenter.Enable();
+            if (_levelInfoPresenter.View is not null)
+            {
+                _levelInfoPresenter.Disable();
+            }
+            _gameController.OnPlayerLoose();
+        }
+
+        public void Exit()
+        {
+            _logger.Log($"{GetType().Name}.{nameof(Exit)}()");
+            _looseMenuPresenter.Disable();
+        }
+
+        private readonly ILogger _logger;
+        IViewFactory _viewFactory;
+        IPresenter<ILooseMenuView> _looseMenuPresenter;
+        IPresenter<IILevelInfoView> _levelInfoPresenter;
+        IGameController _gameController;
+    }
+}
