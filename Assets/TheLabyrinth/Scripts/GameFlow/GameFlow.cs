21ec09d449620f58b8a27e5d735a1aeb7df3a8ed
KarenKrill <vampart1997@gmail.com>
2025-03-28 17:56:20 +0500

    Removed unused game flow events

--- Assets/TheLabyrinth/Scripts/GameFlow/GameFlow.cs
+++ Assets/TheLabyrinth/Scripts/GameFlow/GameFlow.cs
@@ -1,70 +1,15 @@
-using System;
-using UnityEngine;
-using Zenject;
-using KarenKrill.Common.Logging;
-using KarenKrill.TheLabyrinth.StateMachine.Abstractions;
-using KarenKrill.TheLabyrinth.GameFlow.Abstractions;
-
 namespace KarenKrill.TheLabyrinth.GameFlow
 {
-    public class GameFlow : IGameFlow, IDisposable
-    {
-        [Inject]
-        ILogger _logger;
-        [Inject]
-        IStateMachine<GameState> _stateMachine;
-        public GameState State => _stateMachine.State;
-        IStateSwitcher<GameState> _stateSwitcher => _stateMachine.StateSwitcher;
-        public event Action LevelFinish;
-        public event Action GameStart;
-        public event Action LevelPause;
-        public event Action GameEnd;
-        public event Action MainMenuLoad;
-        public event Action LevelLoad;
-        public event Action PlayerWin;
-        public event Action PlayerLoose;
-        public event Action LevelPlay;
+    using Abstractions;
+    using StateMachine.Abstractions;
 
-        private void OnStateEnter(GameState state)
+    public class GameFlow : IGameFlow
+    {
+        public GameState State => _stateSwitcher.State;
+
+        public GameFlow(IStateSwitcher<GameState> stateSwitcher)
         {
-            _logger.Log($"{nameof(GameFlow)} Entered to {state} state");
-            switch (state)
-            {
-                case GameState.GameStart:
-                    GameStart?.Invoke();
-                    break;
-                case GameState.MainMenu:
-                    MainMenuLoad?.Invoke();
-                    break;
-                case GameState.LevelLoad:
-                    LevelLoad?.Invoke();
-                    break;
-                case GameState.LevelFinish:
-                    LevelFinish?.Invoke();
-                    break;
-                case GameState.PauseMenu:
-                    LevelPause?.Invoke();
-                    break;
-                case GameState.WinMenu:
-                    PlayerWin?.Invoke();
-                    break;
-                case GameState.LooseMenu:
-                    PlayerLoose?.Invoke();
-                    break;
-                case GameState.GameEnd:
-                    GameEnd?.Invoke();
-                    break;
-                case GameState.LevelPlay:
-                    LevelPlay?.Invoke();
-                    break;
-                default:
-                    _logger.LogWarning($"{state} state not implemented!");
-                    break;
-            }
-        }
-        private void OnStateExit(GameState state)
-        {
-            //throw new NotImplementedException();
+            _stateSwitcher = stateSwitcher;
         }
         public void FinishLevel() => _stateSwitcher.TransitTo(GameState.LevelFinish);
         public void LoadMainMenu() => _stateSwitcher.TransitTo(GameState.MainMenu);
@@ -75,18 +20,7 @@ namespace KarenKrill.TheLabyrinth.GameFlow
         public void WinGame() => _stateSwitcher.TransitTo(GameState.WinMenu);
         public void LooseGame() => _stateSwitcher.TransitTo(GameState.LooseMenu);
         public void PlayLevel() => _stateSwitcher.TransitTo(GameState.LevelPlay);
-        [Inject]
-        private void Initialize()
-        {
-            _stateMachine.StateEnter += OnStateEnter;
-            _stateMachine.StateExit += OnStateExit;
-            _logger.Log($"GameFlow()");
-        }
-        public void Dispose()
-        {
-            _stateMachine.StateEnter -= OnStateEnter;
-            _stateMachine.StateExit -= OnStateExit;
-            _logger.Log($"GameFlow.Dispose()");
-        }
+
+        private readonly IStateSwitcher<GameState> _stateSwitcher;
     }
 }
--- Assets/TheLabyrinth/Scripts/GameFlow/GameFlowBehaviour.cs
+++ Assets/TheLabyrinth/Scripts/GameFlow/GameFlowBehaviour.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using Zenject;
 
 namespace KarenKrill.TheLabyrinth.GameFlow
@@ -8,110 +7,12 @@ namespace KarenKrill.TheLabyrinth.GameFlow
 
     public class GameFlowBehaviour : MonoBehaviour, IGameFlow
     {
-        [Inject]
-        IGameFlow _gameFlow;
-
         public GameState State => _gameFlow.State;
 
-        public event Action GameStart
+        [Inject]
+        public void Initialize(IGameFlow gameFlow)
         {
-            add
-            {
-                _gameFlow.GameStart += value;
-            }
-            remove
-            {
-                _gameFlow.GameStart -= value;
-            }
-        }
-        public event Action LevelPause
-        {
-            add
-            {
-                _gameFlow.LevelPause += value;
-            }
-            remove
-            {
-                _gameFlow.LevelPause -= value;
-            }
-        }
-        public event Action GameEnd
-        {
-            add
-            {
-                _gameFlow.GameEnd += value;
-            }
-            remove
-            {
-                _gameFlow.GameEnd -= value;
-            }
-        }
-        public event Action MainMenuLoad
-        {
-            add
-            {
-                _gameFlow.MainMenuLoad += value;
-            }
-            remove
-            {
-                _gameFlow.MainMenuLoad -= value;
-            }
-        }
-        public event Action LevelLoad
-        {
-            add
-            {
-                _gameFlow.LevelLoad += value;
-            }
-            remove
-            {
-                _gameFlow.LevelLoad -= value;
-            }
-        }
-        public event Action LevelFinish
-        {
-            add
-            {
-                _gameFlow.LevelFinish += value;
-            }
-            remove
-            {
-                _gameFlow.LevelFinish -= value;
-            }
-        }
-        public event Action PlayerWin
-        {
-            add
-            {
-                _gameFlow.PlayerWin += value;
-            }
-            remove
-            {
-                _gameFlow.PlayerWin -= value;
-            }
-        }
-        public event Action PlayerLoose
-        {
-            add
-            {
-                _gameFlow.PlayerLoose += value;
-            }
-            remove
-            {
-                _gameFlow.PlayerLoose -= value;
-            }
-        }
-
-        public event Action LevelPlay
-        {
-            add
-            {
-                _gameFlow.LevelPlay += value;
-            }
-            remove
-            {
-                _gameFlow.LevelPlay -= value;
-            }
+            _gameFlow = gameFlow;
         }
 
         public void FinishLevel() => _gameFlow.FinishLevel();
@@ -123,5 +24,7 @@ namespace KarenKrill.TheLabyrinth.GameFlow
         public void WinGame() => _gameFlow.WinGame();
         public void PlayLevel() => _gameFlow.PlayLevel();
         public void EndGame() => _gameFlow.EndGame();
+
+        IGameFlow _gameFlow;
     }
 }
