e1c9d9280852bc21c86f18ffe376574fc788b508
KarenKrill <vampart1997@gmail.com>
2025-03-28 17:42:51 +0500

    Added logs to main menu game state

--- Assets/TheLabyrinth/Scripts/StateMachine/GameStates/MainMenuState.cs
+++ Assets/TheLabyrinth/Scripts/StateMachine/GameStates/MainMenuState.cs
@@ -1,4 +1,6 @@
-﻿namespace KarenKrill.TheLabyrinth.GameStates
+﻿using UnityEngine;
+
+namespace KarenKrill.TheLabyrinth.GameStates
 {
     using Common.UI.Presenters.Abstractions;
     using Common.UI.Views.Abstractions;
@@ -10,23 +12,27 @@
     {
         public GameState State => GameState.MainMenu;
 
-        public MainMenuState(IPresenter<IMainMenuView> mainMenuPresenter, IViewFactory viewFactory)
+        public MainMenuState(ILogger logger, IPresenter<IMainMenuView> mainMenuPresenter, IViewFactory viewFactory)
         {
+            _logger = logger;
             _mainMenuPresenter = mainMenuPresenter;
             _viewFactory = viewFactory;
         }
 
         public void Enter()
         {
+            _logger.Log($"{nameof(MainMenuState)}.{nameof(Enter)}()");
             _mainMenuPresenter.View ??= _viewFactory.Create<IMainMenuView>();
             _mainMenuPresenter.Enable();
         }
 
         public void Exit()
         {
+            _logger.Log($"{nameof(MainMenuState)}.{nameof(Exit)}()");
             _mainMenuPresenter.Disable();
         }
-
+        
+        private readonly ILogger _logger;
         private readonly IPresenter<IMainMenuView> _mainMenuPresenter;
         private readonly IViewFactory _viewFactory;
     }
