74184f52d08170d962a605300b84efdef0d281d0
KarenKrill <vampart1997@gmail.com>
2025-04-08 02:05:07 +0500

    Added common move strategy behaviour

---
+++ Assets/TheLabyrinth/Scripts/Movement/OrdinaryMoveBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace KarenKrill.TheLabyrinth.Movement
+{
+    using Abstractions;
+
+    public class OrdinaryMoveBehaviour : MonoBehaviour, IMoveStrategy
+    {
+        public bool UsePhysics { get => _usePhysics; set => _usePhysics = value; }
+
+        [SerializeField]
+        private bool _usePhysics;
+    }
+}
