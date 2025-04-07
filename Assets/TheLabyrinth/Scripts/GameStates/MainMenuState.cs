72d226956783b1867605a6f8c3e6847e8a6f2ac2
KarenKrill <vampart1997@gmail.com>
2025-04-08 03:11:10 +0500

    Disable LevelInfo when return to main menu

--- Assets/TheLabyrinth/Scripts/GameStates/MainMenuState.cs
+++ Assets/TheLabyrinth/Scripts/GameStates/MainMenuState.cs
@@ -12,16 +12,24 @@ namespace KarenKrill.TheLabyrinth.GameStates
     {
         public GameState State => GameState.MainMenu;
 
-        public MainMenuState(ILogger logger, IPresenter<IMainMenuView> mainMenuPresenter, IViewFactory viewFactory)
+        public MainMenuState(ILogger logger,
+            IPresenter<IMainMenuView> mainMenuPresenter,
+            IPresenter<IILevelInfoView> levelInfoPresenter,
+            IViewFactory viewFactory)
         {
             _logger = logger;
             _mainMenuPresenter = mainMenuPresenter;
+            _levelInfoPresenter = levelInfoPresenter;
             _viewFactory = viewFactory;
         }
         public void Enter()
         {
             _logger.Log($"{nameof(MainMenuState)}.{nameof(Enter)}()");
             _mainMenuPresenter.View ??= _viewFactory.Create<IMainMenuView>();
+            if (_levelInfoPresenter.View is not null)
+            {
+                _levelInfoPresenter.Disable();
+            }
             _mainMenuPresenter.Enable();
         }
         public void Exit()
@@ -32,6 +40,7 @@ namespace KarenKrill.TheLabyrinth.GameStates
         
         private readonly ILogger _logger;
         private readonly IPresenter<IMainMenuView> _mainMenuPresenter;
+        private readonly IPresenter<IILevelInfoView> _levelInfoPresenter;
         private readonly IViewFactory _viewFactory;
     }
 }
