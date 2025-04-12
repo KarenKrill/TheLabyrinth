881c051edaf1a0ee6f2e45a3ac2f9507f765a9ad
KarenKrill <vampart1997@gmail.com>
2025-04-12 21:53:26 +0500

    Added abstract MoveBehaviour

---
+++ Assets/TheLabyrinth/Scripts/Abstractions/Movement/MoveBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace KarenKrill.TheLabyrinth.Movement
+{
+    using Abstractions;
+
+    public abstract class MoveBehaviour : MonoBehaviour, IMoveStrategy
+    {
+    }
+}
