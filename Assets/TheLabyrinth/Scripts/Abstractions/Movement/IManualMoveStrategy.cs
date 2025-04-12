ff2cb2f2ce510e6d0c868455f54498a47db22502
KarenKrill <vampart1997@gmail.com>
2025-04-12 22:00:08 +0500

    Added Move method in ManualMoveStrategy interface

--- Assets/TheLabyrinth/Scripts/Abstractions/Movement/IManualMoveStrategy.cs
+++ Assets/TheLabyrinth/Scripts/Abstractions/Movement/IManualMoveStrategy.cs
@@ -1,6 +1,9 @@
-﻿namespace KarenKrill.TheLabyrinth.Movement.Abstractions
+﻿using UnityEngine;
+
+namespace KarenKrill.TheLabyrinth.Movement.Abstractions
 {
     public interface IManualMoveStrategy : IMoveStrategy
     {
+        public void Move(Vector3 destination);
     }
 }
