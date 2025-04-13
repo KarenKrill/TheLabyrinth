63cb7be0e972bf316a6bf177cc09083d2e9780a8
KarenKrill <vampart1997@gmail.com>
2025-04-13 23:20:32 +0500

    Added move strategy switching on level Load/Unload

--- Assets/TheLabyrinth/Scripts/GameFlow/LoadLevelManager.cs
+++ Assets/TheLabyrinth/Scripts/GameFlow/LoadLevelManager.cs
@@ -6,6 +6,7 @@ using Zenject;
 namespace KarenKrill.TheLabyrinth.GameFlow
 {
     using Abstractions;
+    using Movement.Abstractions;
     using MazeGeneration;
     using Movement;
 
@@ -19,9 +20,17 @@ namespace KarenKrill.TheLabyrinth.GameFlow
 #nullable restore
 
         [Inject]
-        public void Initialize(ILogger logger)
+        public void Initialize(ILogger logger,
+            IPlayerMoveController playerMoveController,
+            IManualMoveStrategy manualMoveStrategy,
+            IPhysicMoveStrategy physicMoveStrategy,
+            IPlayerInputMoveStrategy playerInputMoveStrategy)
         {
             _logger = logger;
+            _playerMoveController = playerMoveController;
+            _manualMoveStrategy = manualMoveStrategy;
+            _physicMoveStrategy = physicMoveStrategy;
+            _playerInputMoveStrategy = playerInputMoveStrategy;
         }
 
         public void Reset()
@@ -40,26 +49,27 @@ namespace KarenKrill.TheLabyrinth.GameFlow
         {
             _logger.Log($"{nameof(LoadLevelManager)}.{nameof(LoadLevelCoroutine)}");
             //Time.timeScale = 0;
-            _playerController.LockMovement(xAxis: true, yAxis: true, zAxis: true);
+            _playerMoveController.MoveStrategy = _manualMoveStrategy;
             yield return _mazeBuilder.RebuildCoroutine();
             var playerSpawnPoint = _mazeBuilder.GetCellCenter(_mazeBuilder.Levels - 1, 0);
-            _playerController.LockMovement(xAxis: true, yAxis: false, zAxis: true);
-            yield return _playerController.ForcedMove(new Vector3(playerSpawnPoint.x, 100, playerSpawnPoint.y));
+            _manualMoveStrategy.Move(new Vector3(playerSpawnPoint.x, 100, playerSpawnPoint.y));
+            _playerMoveController.MoveStrategy = _physicMoveStrategy;
             if (_exitPointTransform != null)
             {
                 var exitCellCenter = _mazeBuilder.GetCellCenter(0, 0);
                 _exitPointTransform.position = new Vector3(exitCellCenter.x, _exitPointTransform.position.y, exitCellCenter.y);
             }
-            yield return new WaitForSeconds(0.5f); // wait while player isn't fall (stucks in air)
+            yield return null; // wait frame for CharacterController.IsGrounded
             yield return new WaitUntil(() => _playerController.IsGrounded);// wait until player isn't grounded
-            _logger.Log($"{nameof(LoadLevelManager)}.{nameof(LoadLevelCoroutine)} ends, now level plays");
+            _playerMoveController.MoveStrategy = _playerInputMoveStrategy;
             LevelLoaded?.Invoke();
         }
         private IEnumerator FinishLevelCoroutine()
         {
             //Time.timeScale = 0;
-            _playerController.LockMovement();
-            yield return _playerController.ForcedMove(new Vector3(0, 1, 60));
+            _playerMoveController.MoveStrategy = _manualMoveStrategy;
+            _manualMoveStrategy.Move(new Vector3(0, 1, 60));
+            yield return null;
             _mazeBuilder.Levels = _mazeLevelsCount < _mazeMaxLevelsCount ? ++_mazeLevelsCount : _mazeMaxLevelsCount;
             LevelUnloaded?.Invoke();
         }
@@ -76,6 +86,10 @@ namespace KarenKrill.TheLabyrinth.GameFlow
         private int _mazeMaxLevelsCount = 13;
 
         private ILogger _logger;
+        private IPlayerMoveController _playerMoveController;
+        private IManualMoveStrategy _manualMoveStrategy;
+        private IPhysicMoveStrategy _physicMoveStrategy;
+        private IPlayerInputMoveStrategy _playerInputMoveStrategy;
         private int _mazeLevelsCount = 0;
     }
 }
