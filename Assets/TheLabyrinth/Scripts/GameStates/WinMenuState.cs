41de5231e9b1fd13403b9b331f9b1592178bbb0a
KarenKrill <vampart1997@gmail.com>
2025-04-15 01:12:30 +0500

    Reset player move strategy on game win/lose

--- Assets/TheLabyrinth/Scripts/GameStates/WinMenuState.cs
+++ Assets/TheLabyrinth/Scripts/GameStates/WinMenuState.cs
@@ -6,6 +6,7 @@ namespace KarenKrill.TheLabyrinth.GameStates
     using Common.UI.Presenters.Abstractions;
     using Common.UI.Views.Abstractions;
     using GameFlow.Abstractions;
+    using Movement.Abstractions;
     using UI.Views.Abstractions;
 
     public class WinMenuState : IStateHandler<GameState>
@@ -16,17 +17,20 @@ namespace KarenKrill.TheLabyrinth.GameStates
             IViewFactory viewFactory,
             IPresenter<IWinMenuView> winMenuPresenter,
             IPresenter<IILevelInfoView> levelInfoPresenter,
-            IGameController gameController)
+            IGameController gameController,
+            IPlayerMoveController playerMoveController)
         {
             _logger = logger;
             _viewFactory = viewFactory;
             _winMenuPresenter = winMenuPresenter;
             _levelInfoPresenter = levelInfoPresenter;
             _gameController = gameController;
+            _playerMoveController = playerMoveController;
         }
         public void Enter()
         {
             _logger.Log($"{GetType().Name}.{nameof(Enter)}()");
+            _playerMoveController.MoveStrategy = null;
             _winMenuPresenter.View ??= _viewFactory.Create<IWinMenuView>();
             _winMenuPresenter.Enable();
             if (_levelInfoPresenter.View is not null)
@@ -46,5 +50,6 @@ namespace KarenKrill.TheLabyrinth.GameStates
         private readonly IPresenter<IWinMenuView> _winMenuPresenter;
         private readonly IPresenter<IILevelInfoView> _levelInfoPresenter;
         private readonly IGameController _gameController;
+        private readonly IPlayerMoveController _playerMoveController;
     }
 }
