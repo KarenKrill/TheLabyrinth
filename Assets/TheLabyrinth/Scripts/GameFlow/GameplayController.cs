dad21a7e3c8e78ab4a239fd7166837e8a372ecce
KarenKrill <vampart1997@gmail.com>
2025-04-18 01:45:19 +0500

    Removed LevelController part from GameplayController

--- Assets/TheLabyrinth/Scripts/GameFlow/GameplayController.cs
+++ Assets/TheLabyrinth/Scripts/GameFlow/GameplayController.cs
@@ -8,21 +8,13 @@ namespace KarenKrill.TheLabyrinth.GameFlow
     using Common.StateSystem.Abstractions;
     using Common.GameInfo.Abstractions;
 
-    public class GameplayController : MonoBehaviour, ITimeLimitedLevelInfoProvider, IGameController, IGameInfoProvider
+    public class GameplayController : MonoBehaviour, IGameController, IGameInfoProvider
     {
 #nullable enable
         public int CurrentLevelNumber => _PassedLevels;
         public string? CurrentLevelName => null;
-        public float MaxCompleteTime => _timeOnCurrentLevel;
-        public float RemainingTime => _TimeLeft;
-        public float WarningTime => _warningLeftTime;
-        public float LastWarningTime => _lastWarningLeftTime;
 
         public event Action? CurrentLevelChanged;
-        public event Action<float>? MaxCompleteTimeChanged;
-        public event Action<float>? RemainingTimeChanged;
-        public event Action<float>? WarningTimeChanged;
-        public event Action<float>? LastWarningTimeChanged;
 #nullable restore
 
         [Inject]
@@ -38,27 +30,12 @@ namespace KarenKrill.TheLabyrinth.GameFlow
         public void OnGameEnd() { }
         public void OnLevelLoad()
         {
-            _isLevelWasPaused = false;
         }
         public void OnLevelPlay()
         {
-            if (_isLevelWasPaused)
-            {
-                _isLevelWasPaused = false;
-            }
-            else
-            {
-                _timeOnCurrentLevel = Mathf.Round(_levelTimeFactor * Mathf.Sqrt(_loadLevelManager.TotalMazeCellsCount) / 5) * 5;
-                OnMaxCompleteTimeChanged();
-                if (_PassedLevels <= _gameLevelsCount) // game not ended
-                {
-                    _TimeLeft = _timeOnCurrentLevel;
-                }
-            }
         }
         public void OnLevelPause()
         {
-            _isLevelWasPaused = true;
         }
         public void OnLevelFinish()
         {
@@ -80,39 +57,10 @@ namespace KarenKrill.TheLabyrinth.GameFlow
         private LoadLevelManager _loadLevelManager;
         [SerializeField]
         private int _gameLevelsCount = 13;
-        [SerializeField]
-        private float _levelTimeFactor = 5f;
-        [SerializeField, Range(0, 1)]
-        private float _warningLeftTime = 0.3f;
-        [SerializeField, Range(0, 1)]
-        private float _lastWarningLeftTime = 0.1f;
 
         private IGameFlow _gameFlow;
         private IManagedStateMachine<GameState> _managedStateMachine;
 
-        private float _timeOnCurrentLevel;
-        private bool _isLevelWasPaused = false;
-
-        private float _timeLeft;
-        private float _TimeLeft
-        {
-            get => _timeLeft;
-            set
-            {
-                if (_timeLeft != value)
-                {
-                    if (MathF.Round(value, 1) != MathF.Round(_timeLeft, 1))
-                    {
-                        _timeLeft = value;
-                        OnRemainingTimeChanged();
-                    }
-                    else
-                    {
-                        _timeLeft = value;
-                    }
-                }
-            }
-        }
         private int _passedLevels = 0;
         private int _PassedLevels
         {
@@ -137,50 +85,19 @@ namespace KarenKrill.TheLabyrinth.GameFlow
                 UpdateLeftLevelTime();
             }
         }
-        private void OnValidate()
-        {
-            if (_warningLeftTime < _lastWarningLeftTime)
-            {
-                _warningLeftTime = _lastWarningLeftTime;
-            }
-            OnWarningTimeChanged();
-            OnLastWarningTimeChanged();
-        }
 
         private void UpdateLeftLevelTime()
         {
-            _TimeLeft = _TimeLeft > Time.deltaTime ? _TimeLeft - Time.deltaTime : 0;
-            if (_TimeLeft == 0)
-            {
-                _gameFlow.LooseGame();
-            }
         }
         private void ResetToDefaults()
         {
-            _TimeLeft = 0;
             _PassedLevels = 1;
             OnCurrentLevelChanged();
         }
         
-        private void OnMaxCompleteTimeChanged()
-        {
-            MaxCompleteTimeChanged?.Invoke(MaxCompleteTime);
-        }
-        private void OnRemainingTimeChanged()
-        {
-            RemainingTimeChanged?.Invoke(RemainingTime);
-        }
         private void OnCurrentLevelChanged()
         {
             CurrentLevelChanged?.Invoke();
-        }
-        private void OnWarningTimeChanged()
-        {
-            WarningTimeChanged?.Invoke(_warningLeftTime);
-        }
-        private void OnLastWarningTimeChanged()
-        {
-            LastWarningTimeChanged?.Invoke(_lastWarningLeftTime);
         }
     }
 }
