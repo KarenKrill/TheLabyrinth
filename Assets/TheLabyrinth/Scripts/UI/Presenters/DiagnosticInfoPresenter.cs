cdd26bb18883408bad9f8dc443cb9032a8668b38
KarenKrill <vampart1997@gmail.com>
2025-04-22 21:10:23 +0500

    Added diagnostic info presenter

---
+++ Assets/TheLabyrinth/Scripts/UI/Presenters/DiagnosticInfoPresenter.cs
@@ -0,0 +1,33 @@
+﻿namespace KarenKrill.TheLabyrinth.UI.Presenters
+{
+    using Common.Diagnostics.Abstractions;
+    using Common.UI.Presenters.Abstractions;
+    using Views.Abstractions;
+
+    public class DiagnosticInfoPresenter : IPresenter<IDiagnosticInfoView>
+    {
+        public IDiagnosticInfoView View { get; set; }
+
+        public DiagnosticInfoPresenter(IDiagnosticsProvider diagnosticsProvider)
+        {
+            _diagnosticsProvider = diagnosticsProvider;
+        }
+        public void Enable()
+        {
+            OnPerfomanceInfoChanged(_diagnosticsProvider.PerfomanceInfo);
+            _diagnosticsProvider.PerfomanceInfoChanged += OnPerfomanceInfoChanged;
+            View.Show();
+        }
+        public void Disable()
+        {
+            View.Close();
+            _diagnosticsProvider.PerfomanceInfoChanged -= OnPerfomanceInfoChanged;
+        }
+
+        private readonly IDiagnosticsProvider _diagnosticsProvider;
+        private void OnPerfomanceInfoChanged(PerfomanceInfo perfomanceInfo)
+        {
+            View.FpsText = $"FpsAvg: {perfomanceInfo.FpsAverage:0.0}";
+        }
+    }
+}
