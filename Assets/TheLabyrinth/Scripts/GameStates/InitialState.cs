4532175458ee5eb3aab8d7a0eed95e4b44d08f5d
KarenKrill <vampart1997@gmail.com>
2025-04-23 21:19:07 +0500

    Added diagnostics info activation on game settings changes

--- Assets/TheLabyrinth/Scripts/GameStates/InitialState.cs
+++ Assets/TheLabyrinth/Scripts/GameStates/InitialState.cs
@@ -3,16 +3,27 @@
 namespace KarenKrill.TheLabyrinth.GameStates
 {
     using Common.StateSystem.Abstractions;
+    using Common.UI.Presenters.Abstractions;
+    using Common.UI.Views.Abstractions;
     using GameFlow.Abstractions;
+    using TheLabyrinth.Abstractions;
+    using UI.Views.Abstractions;
 
     public class InitialState : IStateHandler<GameState>
     {
         public GameState State => GameState.Initial;
 
-        public InitialState(ILogger logger, IGameFlow gameFlow)
+        public InitialState(ILogger logger,
+            IGameFlow gameFlow,
+            IPresenter<IDiagnosticInfoView> diagnosticInfoPresenter,
+            IViewFactory viewFactory,
+            GameSettings gameSettings)
         {
             _logger = logger;
             _gameFlow = gameFlow;
+            _diagnosticInfoPresenter = diagnosticInfoPresenter;
+            _viewFactory = viewFactory;
+            gameSettings.ShowFpsChanged += OnShowFpsChanged;
         }
         public void Enter(GameState prevState)
         {
@@ -26,5 +37,20 @@ namespace KarenKrill.TheLabyrinth.GameStates
 
         private readonly ILogger _logger;
         private readonly IGameFlow _gameFlow;
+        private readonly IPresenter<IDiagnosticInfoView> _diagnosticInfoPresenter;
+        private readonly IViewFactory _viewFactory;
+
+        private void OnShowFpsChanged(bool state)
+        {
+            if (state)
+            {
+                _diagnosticInfoPresenter.View ??= _viewFactory.Create<IDiagnosticInfoView>();
+                _diagnosticInfoPresenter.Enable();
+            }
+            else if (_diagnosticInfoPresenter.View is not null)
+            {
+                _diagnosticInfoPresenter.Disable();
+            }
+        }
     }
 }
