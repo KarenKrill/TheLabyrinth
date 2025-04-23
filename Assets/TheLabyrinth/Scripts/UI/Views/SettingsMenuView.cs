22cffe86e2bc13c4ec4e8eeb9469122c351d6b14
KarenKrill <vampart1997@gmail.com>
2025-04-23 19:36:18 +0500

    Added settings view implementation

---
+++ Assets/TheLabyrinth/Scripts/UI/Views/SettingsMenuView.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KarenKrill.TheLabyrinth.UI.Views
+{
+    using Abstractions;
+    using Common.UI.Views;
+
+    public class SettingsMenuView : ViewBehaviour, ISettingsMenuView
+    {
+        #region Graphics
+
+        #endregion
+
+        #region Diagnostic
+        public bool ShowFps { get => _showFpsToggle.isOn; set => _showFpsToggle.isOn = value; }
+        #endregion
+
+#nullable enable
+        public event Action? Apply;
+        public event Action? Cancel;
+#nullable restore
+
+        [SerializeField]
+        private Toggle _showFpsToggle;
+        [SerializeField]
+        private Button _applyButton;
+        [SerializeField]
+        private Button _cancelButton;
+
+        private void OnEnable()
+        {
+            _applyButton.onClick.AddListener(OnApplyButtonClicked);
+            _cancelButton.onClick.AddListener(OnCancelButtonClicked);
+        }
+        private void OnDisable()
+        {
+            _applyButton.onClick.RemoveListener(OnApplyButtonClicked);
+            _cancelButton.onClick.RemoveListener(OnCancelButtonClicked);
+        }
+
+        private void OnApplyButtonClicked() => Apply?.Invoke();
+        private void OnCancelButtonClicked() => Cancel?.Invoke();
+    }
+}
